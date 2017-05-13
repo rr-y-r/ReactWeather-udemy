e9d1e2b3ee8398a3672581670080761a1585a710
ryan rynaldo <rr.y.r01@gmail.com>
2017-05-13 15:37:44 +0700

    styling main and example pages

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 var Example = () => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>welcome to example page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example location to try out :</p>
+      <ol>
+        <li>
+          <NavLink to='/?location=BandaAceh'>Banda Aceh, NAD</NavLink>
+        </li>
+        <li>
+          <NavLink to='/?location=Rio'>Rio, Brazil</NavLink>
+        </li>
+      </ol>
     </div>
   );
 };
