83861af73caff1e471ad17cb3e598fd3285d9b28
ryan rynaldo <rr.y.r01@gmail.com>
2017-05-13 15:44:52 +0700

    styling about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>welcome to about pages</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React.
+        I have built this for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react"> - This was the JavaScript framework used.</a>
+        </li>
+        <li>
+          <a href="https://openweathermap.org"> - something</a>
+        </li>
+      </ul>
     </div>
   );
 };
